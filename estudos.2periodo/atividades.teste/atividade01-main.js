f968615d42d39895fb070f85175db7669f1f65f4
Ítalo <163360684+iti0027@users.noreply.github.com>
2024-08-20 23:01:49 +0000

    atualizando o js e criando ocss

--- estudos.2periodo/atividades.teste/atividade01-main.js
+++ estudos.2periodo/atividades.teste/atividade01-main.js
@@ -1,33 +1,53 @@
 window.alert('Bem vindo ao nosso site! :p');
 
 function verificar(){
-    var nome = document.querySelector('input.nome');
+    var nome = document.querySelector('input.nome').value;
     var Nresultado = document.querySelector('div.nomeResultado');
 
-    var idade = document.querySelector('input.idade');
+    var idade = document.querySelector('input.idade').value;
     var Iresultado = document.querySelector('div.idadeResultado');
 
-    var cidade = document.querySelector('input.cidade');
+    var cidade = document.querySelector('input.cidade').value;
     var Cresultado = document.querySelector('div.cidadeResultado')
 
-    var petTipo = document.querySelector('input.petTipo');
-    var petName = document.querySelector('input.petName');
+    var petTipo = document.querySelector('input.petTipo').value;
+    var pTResultado = document.querySelector('div.pTResultado');
 
-    if (nome == 0){
+    var petName = document.querySelector('input.petName').value;
+    var pNResultado = document.querySelector('div.pNResultado');
+
+    if (nome == ""){
         Nresultado.innerHTML = 'Por favor, insira o seu nome.'
     } else{
         Nresultado.innerHTML = `Muito prazer em conhece-lo(a) <strong>${nome}</strong>!`
     }
 
-    if(idade == 0){
+    if(idade == ""){
         Iresultado.innerHTML = 'Por favor, insira a sua idade.'
     } else{
-        Iresultado.innerHTML = `Hmm então quer dizer que você tem ${idade} não seu <strong>${nome}</strong> bom saber.`
+        Iresultado.innerHTML = `Hmm então quer dizer que você tem ${idade} não é seu <strong>${nome}</strong> bom saber.`
     }
 
-    if (cidade == 0 ){
+    if (cidade == ""){
         Cresultado.innerHTML = 'Por favor, insira a cidade em que nasceu.'
     } else{
         Cresultado.innerHTML = `Que legal! Conheço ${cidade} muito bem. Sempre ia para lá quando criança. Vamos marcar de nos encontrar qualquer dia por lá <strong>${nome}</strong>, o que acha?`
+    }
+
+    if(petTipo ==""){
+        pTResultado.innerHTML = 'Por favor, insira o tipo do deu pet.'
+    } else if (petTipo == "cachorro"){
+        pTResultado.innerHTML = `Que massa! Eu também tenho um Cachorro, aliás, tenho duas cachorras e seus nomes são Perola e Agatha.`
+    } else if (petTipo == "gato"){
+        pTResultado.innerHTML = `Que massa! Eu também tenho um cachorro, aliás, tenho uma gata chamada Safira.`
+    } else {
+        pTResultado.innerHTML = `Muito legal! Ainda não tive o prazer de cuidar desse tipo de pet, mas certeza que deve ser legal.`
+    }
+
+
+    if (petName ==""){
+        pNResultado.innerHTML ='Por favor, diga o nome do seu pet.'
+    } else{
+        pNResultado.innerHTML = 'Que nome maneiro ksksksks'
     }
 }
