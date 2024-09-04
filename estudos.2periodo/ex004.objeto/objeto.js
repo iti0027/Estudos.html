327f52f2f684a35f7591c564ea9547f1048b6be3
Ítalo <163360684+iti0027@users.noreply.github.com>
2024-09-04 21:35:02 +0000

    atividade de objetos

--- estudos.2periodo/ex004.objeto/objeto.js
+++ estudos.2periodo/ex004.objeto/objeto.js
@@ -1,1 +1,20 @@
-window.alert('teste');
+function verificar(){
+    const dados = document.querySelectorAll('#matriculainput, #cpfinput, #idadeinput, #nomeinput').values;
+    const res = document.querySelector('#resultado');
+
+    if (dados.idade % 2 !== 0){
+        window.alert('Por favor, insira um número inteiro.');
+        return;
+    }
+
+    if (dados.nome === ''){
+        window.alert('Por favor, preencha o campo nome.');
+        return;
+    }
+
+    if (!matricula || !cpf || !idade || !nome) {
+        window.alert('Por favor, preencha todos os campos.');
+        return
+    }
+    res.innerHTML = `${dados}`
+};
