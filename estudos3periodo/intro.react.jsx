293de5359d529a4aa9988828a3235744d06f0a99
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-02-11 00:32:30 +0000

    intro react (fiz nada)

---
+++ estudos3periodo/intro.react.jsx
@@ -0,0 +1,15 @@
+
+function MyButton () {
+    return (
+        <button>I'm fuck button</button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my fuck App</h1>
+            <MyButton/>
+        </div>
+    );
+}
