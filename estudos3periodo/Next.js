9ac941140b7bf4344adf5255389c3ef8532514a9
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-02-14 00:38:13 +0000

    aprendo react

---
+++ estudos3periodo/Next.js
@@ -0,0 +1,4 @@
+export default function home(){
+    return <h1>Óla Turma!!</h1>;
+     return <p>Primera aula de react nesse cacetinho!</p>;
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+
+  const aula = 1;
+  return (
+    <div>
+    <h1>Olá Turma!!</h1>
+    <p>Éssa é a minha {aula}ª aula de React</p>
+   </div>
+  );
+
+} 
