b3964c28512fa19124aa0d4675009717b9bc4194
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-02-25 00:36:09 +0000

    criando um código para gerar um número aleatório

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,12 +1,34 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
+
+function gerarNumAleatorio () {
+  return Math.floor(Math.random()* 100) + 1;
+}
+
 
 export default function Home() {
-
-  const aula = 1;
+  const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(0)
+  const aula = 2;
   return (
     <div>
     <h1>Olá Turma!!</h1>
     <p>Éssa é a minha {aula}ª aula de React</p>
+
+    <button onClick={ () => setHide(!hide)}> {hide ? "Show" : "Hide"}</button>
+    <button onClick={() => setNumAleat(gerarNumAleatorio())}>
+      Aleatório: {numAleat};  
+    </button>
+
+    {! hide &&(
+      <>
+      <h1>Teste</h1>
+      <h1>Teste 2</h1>
+      </>
+    )
+
+    }
    </div>
   );
 
