6f68010a121dacf84d40570092e627168738901d
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-03-14 22:48:47 +0000

    lol

---
+++ java3periodo/pessoas/Pessoa.java
@@ -0,0 +1,11 @@
+package pessoas;
+
+public class Pessoa {
+    private String nome;
+    private short idade;
+
+    public pessoa (String nome, short idade){
+        this.nome = nome;
+        this. idade = idade;
+    }
+}
